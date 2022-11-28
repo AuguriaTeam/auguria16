d22e7ff6607f8267c251c7ff18834761efc7ac99
AG-AbdellahJ <85180273+AG-AbdellahJ@users.noreply.github.com>
2022-11-28 13:39:26 +0100

    Add file

---
+++ addons_generic/auguria_iban_on_invoice/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021 - Auguria (<https://www.auguria.fr>).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from . import res_company
+from . import res_partner_bank
+from . import res_partner
+from . import account_move
+
+from . import sale_order
